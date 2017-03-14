9011ba21ef1f9bc46fee048e82a80606f92f70ab
teemoleary <727630072@qq.com>
2017-03-14 17:03:08 +0800

    使用ButterKnife生成id并讲其在onDestroy中释放，讲布局外层改为RadioGroup

--- app/src/main/java/com/example/lr/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/lr/fulicenter/ui/activity/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.lr.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
 
 import com.example.lr.fulicenter.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.fragment_container)
+    FrameLayout fragmentContainer;
+    @BindView(R.id.layout_new_good)
+    RadioButton layoutNewGood;
+    @BindView(R.id.layout_boutique)
+    RadioButton layoutBoutique;
+    @BindView(R.id.layout_category)
+    RadioButton layoutCategory;
+    @BindView(R.id.layout_cart)
+    RadioButton layoutCart;
+    @BindView(R.id.personal)
+    RadioButton personal;
+    Unbinder bind = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bind = ButterKnife.bind(this);
     }
-    public void onCheckedChange(View view){}
+
+    public void onCheckedChange(View view) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind != null) {
+            bind.unbind();
+        }
+    }
 }
