9c7b83f478e9e7ca2a144ac02f215b99f66cf88b
teemoleary <727630072@qq.com>
2017-03-14 14:51:44 +0800

    添加闪屏界面，并在两秒后跳转到MainActivity

--- app/src/main/java/com/example/lr/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/lr/fulicenter/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lr.fulicenter;
+package com.example.lr.fulicenter.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lr.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/lr/fulicenter/ui/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.lr.fulicenter.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lr.fulicenter.R;
+
+public class SplashActivity extends AppCompatActivity {
+    int time=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        },time);
+    }
+}
