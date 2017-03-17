f20ea03ece59ac416125019ba3792b908f5e99e3
teemoleary <727630072@qq.com>
2017-03-17 15:20:51 +0800

    修改二级页面的网络接口

--- app/src/main/java/com/example/lr/fulicenter/model/net/NewGoodsModel.java
+++ app/src/main/java/com/example/lr/fulicenter/model/net/NewGoodsModel.java
@@ -14,8 +14,12 @@ public class NewGoodsModel  extends ModelBase implements INewGoodsModel {
 
     @Override
     public void loadData(Context context, int catId, int pageId, OnCompleteListener<NewGoodsBean[]> listener) {
+        String url = I.REQUEST_FIND_NEW_BOUTIQUE_GOODS;
+        if (catId > 0) {
+            url = I.REQUEST_FIND_GOODS_DETAILS;
+        }
         OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(context);
-        utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
+        utils.setRequestUrl(url)
                 .addParam(I.NewAndBoutiqueGoods.CAT_ID,String.valueOf(catId))
                 .addParam(I.PAGE_ID,String.valueOf(pageId))
                 .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE_DEFAULT))
