fdc6234e1f374b559bc6bab1fd54429c3b250868
teemoleary <727630072@qq.com>
2017-03-20 17:14:48 +0800

    实现注册功能，使用startForResult方法返回用户名到登录界面

--- app/src/main/java/com/example/lr/fulicenter/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/lr/fulicenter/ui/activity/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.lr.fulicenter.ui.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,7 +9,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.lr.fulicenter.R;
+import com.example.lr.fulicenter.application.I;
+import com.example.lr.fulicenter.model.bean.Result;
+import com.example.lr.fulicenter.model.bean.User;
+import com.example.lr.fulicenter.model.net.IUserModel;
+import com.example.lr.fulicenter.model.net.OnCompleteListener;
+import com.example.lr.fulicenter.model.net.UserModel;
+import com.example.lr.fulicenter.model.utils.CommonUtils;
+import com.example.lr.fulicenter.model.utils.L;
 import com.example.lr.fulicenter.model.utils.MFGT;
+import com.example.lr.fulicenter.model.utils.ResultUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,11 +33,23 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.etPassword)
     EditText etPassword;
 
+    LoginActivity mContext;
+    String userName;
+    String password;
+    IUserModel model;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        model = new UserModel();
+        mContext = this;
+        tvHeadTitle.setText("账户登录");
     }
 
     @OnClick({R.id.ivBack, R.id.btnLogin, R.id.btnRegister})
@@ -36,10 +59,84 @@ public class LoginActivity extends AppCompatActivity {
                 MFGT.finish(this);
                 break;
             case R.id.btnLogin:
+                checkedInput();
                 break;
             case R.id.btnRegister:
                 MFGT.gotoRegisterActivity(this);
                 break;
+        }
+    }
+    private void checkedInput() {
+        userName = etUserName.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        if (userName.isEmpty()) {
+            CommonUtils.showShortToast(R.string.user_name_connot_be_empty);
+            etUserName.requestFocus();
+            return;
+        } else if (password.isEmpty()) {
+            CommonUtils.showShortToast(R.string.password_connot_be_empty);
+            etPassword.requestFocus();
+            return;
+        }
+        login();
+    }
+
+    private void login() {
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.logining));
+        pd.show();
+        L.i("userName"+userName+"password"+password);
+        model.login(mContext, userName, password, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String str) {
+                Result result = ResultUtils.getResultFromJson(str, User.class);
+                if (result.getRetCode() !=0) {
+                    CommonUtils.showShortToast(R.string.login_fail);
+                } else /*{
+                    if (result.isRetMsg()) {
+                        User user= (User) result.getRetData();
+                      *//*  String strUser = result.getRetData().toString();
+                        OkHttpUtils<User> utils = new OkHttpUtils<>(mContext);
+                        User user = utils.parseJson(strUser, User.class);*//*
+                        L.e("login"+user.toString());
+
+                        UserDao dao = new UserDao(mContext);
+                        boolean isSuccess= dao.saveUser(user);
+                        if (isSuccess) {
+                            SharedPreferenceUtils.getInstance(mContext).saveUser(user.getMuserName());
+                            FuLiCenterApplication.setUser(user);
+                            MFGT.finish(mContext);
+                            CommonUtils.showLongToast(R.string.login_success);
+                        } else {
+                            CommonUtils.showLongToast(R.string.user_database_error);
+                        }
+                    } else {
+                        if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                            CommonUtils.showLongToast(R.string.login_fail_unknow_user);
+                        } else if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                            CommonUtils.showLongToast(R.string.login_fail_error_password);
+                        }else {
+                            CommonUtils.showLongToast(R.string.login_fail);
+                        }
+                    }
+                }*/
+                pd.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                CommonUtils.showShortToast("login" + error);
+                L.e("login error" + error);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_REGISTER) {
+            String name = data.getStringExtra(I.User.USER_NAME);
+            etUserName.setText(name);
         }
     }
 }
--- app/src/main/java/com/example/lr/fulicenter/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/example/lr/fulicenter/ui/activity/RegisterActivity.java
@@ -1,15 +1,129 @@
 package com.example.lr.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.lr.fulicenter.R;
+import com.example.lr.fulicenter.application.I;
+import com.example.lr.fulicenter.model.bean.Result;
+import com.example.lr.fulicenter.model.net.IUserModel;
+import com.example.lr.fulicenter.model.net.OnCompleteListener;
+import com.example.lr.fulicenter.model.net.UserModel;
+import com.example.lr.fulicenter.model.utils.CommonUtils;
+import com.example.lr.fulicenter.model.utils.L;
+import com.example.lr.fulicenter.model.utils.MFGT;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvHeadTitle)
+    TextView tvHeadTitle;
+    @BindView(R.id.etUserName)
+    EditText etUserName;
+    @BindView(R.id.etNick)
+    EditText etNick;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+    @BindView(R.id.etCheckedPassword)
+    EditText etCheckedPassword;
+    String userName;
+    String userNick;
+    String password;
+    String checkedPassword;
+    RegisterActivity mContext;
+    IUserModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        model = new UserModel();
+        mContext = this;
+        tvHeadTitle.setText("账户注册");
+    }
+
+    @OnClick({R.id.ivBack, R.id.btnRegister_free})
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ivBack:
+                MFGT.finish(this);
+                break;
+            case R.id.btnRegister_free:
+                userName = etUserName.getText().toString().trim();
+                userNick = etNick.getText().toString().trim();
+                password = etPassword.getText().toString().trim();
+                checkedPassword = etCheckedPassword.getText().toString().trim();
+                if (userName == null || userName.length() == 0) {
+                    CommonUtils.showShortToast(R.string.user_name_connot_be_empty);
+                    etUserName.requestFocus();
+                    return;
+                } else if (!userName.matches("[a-zA-Z]\\w{5,15}")) {
+                    CommonUtils.showShortToast(R.string.illegal_user_name);
+                    etUserName.requestFocus();
+                    return;
+                }else if (userNick.isEmpty()) {
+                    CommonUtils.showShortToast(R.string.nick_name_connot_be_empty);
+                    etNick.requestFocus();
+                    return;
+                }else if (password.isEmpty()) {
+                    CommonUtils.showShortToast(R.string.password_connot_be_empty);
+                    etPassword.requestFocus();
+                    return;
+                } else if (checkedPassword.isEmpty()) {
+                    CommonUtils.showShortToast(R.string.confirm_password_connot_be_empty);
+                    etCheckedPassword.requestFocus();
+                    return;
+                }else if (!password.equals(checkedPassword)) {
+                    CommonUtils.showShortToast(R.string.two_input_password);
+                    etCheckedPassword.requestFocus();
+                    return;
+                }
+                register(userName, userNick, password);
+                break;
+        }
+    }
+
+    private void register(final String userName, String userNick, String password) {
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.registering));
+        pd.show();
+        model.register(mContext, userName, userNick, password, new OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                if (result.getRetCode() != 0) {
+                    CommonUtils.showShortToast(R.string.register_fail);
+                } else {
+                    if (result.isRetMsg()) {
+                        CommonUtils.showLongToast(R.string.register_success);
+                        setResult(RESULT_OK, new Intent().putExtra(I.User.USER_NAME, userName));
+                        MFGT.finish(mContext);
+                    } else {
+                        CommonUtils.showLongToast(R.string.register_fail_exists);
+                        etUserName.requestFocus();
+                    }
+                }
+                pd.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showShortToast(error);
+                L.e("register error"+error);
+            }
+        });
     }
 }
